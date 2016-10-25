3c019237516cd07c1a624c43565c88cfa4eaccfc
Ian Agne <ian.d.agne@gmail.com>
2016-10-24 21:09:12 -0500

    Create separate version file

--- lib/administrate/version.rb
+++ lib/administrate/version.rb
@@ -1,10 +1,7 @@
-require "administrate/field/base"
-
 module Administrate
   module Field
     class Image < Administrate::Field::Base
-      class Engine < ::Rails::Engine
-      end
+      VERSION = "0.0.2"
     end
   end
 end
